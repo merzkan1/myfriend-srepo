715533bf66c0e43d232549977fc3a183344d78a7
merzkan <113693032+merzkan1@users.noreply.github.com>
2023-11-26 01:07:35 +0300

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Felsefi Metin</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #000;
+            color: #00ff00;
+            text-align: center;
+            padding: 50px;
+            margin: 0;
+            overflow: hidden;
+        }
+
+        #content {
+            background-color: #000;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 255, 0, 0.1);
+            position: relative;
+            z-index: 2;
+        }
+
+        h1, p {
+            color: #00ff00;
+        }
+
+        span {
+            font-size: 30px;
+            color: #ffd700;
+        }
+
+        code {
+            color: #00ff00;
+            background-color: #000;
+            padding: 5px;
+            border-radius: 3px;
+        }
+    </style>
+</head>
+<body>
+    <div id="content">
+        <h1>Felsefi Metin</h1>
+        <p><code>"Mutluluk huzur umut getirmiştim ama almadılar çiçeklerimden bir tane bile"</code> <span>😈</span></p>
+    </div>
+</body>
+</html>
